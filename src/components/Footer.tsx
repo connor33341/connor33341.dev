3fab3c95bed4bd484793894b7545d95589f76b3c
Connor <107011324+connor33341@users.noreply.github.com>
2025-08-05 17:21:39 -0400

    Update vite.config.ts

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,10 +3,14 @@ import React from 'react';
 const Footer: React.FC = () => {
     return (
         <footer>
-            <p>&copy; {new Date().getFullYear()} My Personal Website. All rights reserved.</p>
+            <p>&copy; {new Date().getFullYear()} connor33341.dev. All rights reserved.</p>
             <nav>
                 <a href="/about">About</a>
                 <a href="/contact">Contact</a>
+                <a href="/services">Services</a>
+                <a>VoxaCommunications</a>
+                <a>StellarisChain</a>
+                <a>HalogenChain</a>
             </nav>
         </footer>
     );
---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+//import laravel from 'laravel-vite-plugin';
+//import { resolve } from 'node:path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    plugins: [
+        react(),
+        tailwindcss(),
+    ],
+    esbuild: {
+        jsx: 'automatic',
+    },
+    build: {
+        rollupOptions: {
+            input: 'src/index.tsx'
+        }
+    },
+    /*resolve: {
+        alias: {
+            'ziggy-js': resolve(__dirname, 'vendor/tightenco/ziggy'),
+        },
+    },*/
+    assetsInclude: ['**/*.md'],
+});
